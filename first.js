f63395037c4a08612c7465d1eeb384589c6863e2
divyanshsingh212007-ai <divyanshsingh212007@gmail.com>
2026-06-19 22:22:31 +0530

    First(js)

---
+++ first.js
@@ -0,0 +1,15 @@
+let stu={
+    name: "Ajay",
+    age: 54,
+    gmail: "xyz@gmail.com",
+}
+console.log(stu.name);
+let a=10;
+let b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(++a);
+console.log(b--);
+console.log(a%b);
